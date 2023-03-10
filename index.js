bef6e6b2e5185c2ca84d3226ec7dac1aefad8124
KALE PRAJWAL DADASO <76198202+prajwalk6739@users.noreply.github.com>
2023-03-10 07:52:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("Hello World!");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontent = document.getElementsByClassName("tab-content");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
